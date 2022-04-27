970c459075ea33a60508fc86fa41c7c263ffd12d
Your Name <you@example.com>
2022-04-27 23:45:40 +0100

    Created new branch created Shoe package Models and created new Kotlin class called Shoe

---
+++ src/main/kotlin/models/Shoe.kt
@@ -0,0 +1,11 @@
+package models
+
+
+
+data class Shoe(val shoeName: String,
+                val shoeSize: Int,
+                val shoeDescription: String,
+                val shoePrice: Double,
+                val shoeType: String,
+                val inCurrentProductLine :Boolean) {
+}
